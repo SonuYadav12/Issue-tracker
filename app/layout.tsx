d923f988f3afe481d2e70248617a40187347c60e
Sonu Kumar Yadav <yadavsonukumar036@gmail.com>
2023-10-24 13:45:52 +0530

    New Issue Page added

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Theme>
-          <NavBar />
-          <main>
+          <NavBar/>
+          <main className='p-5'>
             {children}
           </main>
         </Theme>
